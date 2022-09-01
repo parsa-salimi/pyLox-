116dbb8e4ddb6618fba82f92aacc0b99ea96fc9b
parsa-salimi <parsa2000kurd@gmail.com>
2022-08-31 20:10:22 -0400

    interpreter bells and whistles

--- plox/Interpreter.py
+++ plox/Interpreter.py
@@ -49,11 +49,21 @@ class Interpreter(Expr.Visitor):
             return left - right
         elif(expr.operator.type == TokenType.SLASH): 
             self.checkNumberOperands(expr.operator, left, right)
+            if (right == 0):
+                raise LoxTypeError(expr.operator, "division by zero")
             return left / right
         elif(expr.operator.type == TokenType.STAR): 
             self.checkNumberOperands(expr.operator, left, right)
             return left * right
         elif(expr.operator.type == TokenType.PLUS) : 
+            if (isinstance(left, str) and isinstance(right,str)):
+                return left + right
+            if (isinstance(left, float) and isinstance(right, float)):
+                return left + right
+            if (isinstance(left,str)):
+                return left + self.stringify(right)
+            if (isinstance(right,str)):
+                return self.stringify(left) + right
             return left + right
             raise LoxTypeError(operator, "operands must be numbers or strings")
         elif(expr.operator.type == TokenType.GREATER) : 
